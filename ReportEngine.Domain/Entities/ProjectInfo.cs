a99e13c17e31132e7bb9bd7631778b969c48dc91
DaniilTiunov <114344853+DaniilTiunov@users.noreply.github.com>
2025-07-02 13:52:07 +0500

    Накакал

---
+++ ReportEngine.Domain/Entities/ProjectInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportEngine.Domain.Entities
+{
+    internal class ProjectInfo
+    {
+        string Name { get; set; } 
+        string Description { get; set; }
+    }
+}
---
+++ ReportEngine.Domain/Repositories/BaseRepository.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.CodeAnalysis;
+using ReportEngine.Domain.Repositories.Interfaces;
+using ReportEngine.Domain.Entities;
+
+
+
+namespace ReportEngine.Domain.Repositories
+{
+    internal class BaseRepository : IBaseRepository<Entities.ProjectInfo>
+    {
+        public Task AddAsync(Entities.ProjectInfo entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddAsync(Entities.ProjectInfo entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Entities.ProjectInfo entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Entities.ProjectInfo entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Entities.ProjectInfo>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Entities.ProjectInfo> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Entities.ProjectInfo entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Entities.ProjectInfo entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<IEnumerable<Entities.ProjectInfo>> IBaseRepository<Entities.ProjectInfo>.GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Entities.ProjectInfo> IBaseRepository<Entities.ProjectInfo>.GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
