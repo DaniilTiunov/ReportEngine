51df921eae5bcb7d424fc2e0e91dd66270eca3d9
DaniilTiunov <114344853+DaniilTiunov@users.noreply.github.com>
2025-07-04 23:42:25 +0500

    Убран лишний код, т.к. точка входа находится теперь в StartUp

--- ReportEngine.App/App.cs
+++ ReportEngine.App/App.cs
@@ -4,16 +4,6 @@ namespace ReportEngine.App
 {
     public class App : Application
     {
-        readonly MainWindow _mainWindow;
-
-        public App(MainWindow mainWindow)
-        {
-            _mainWindow = mainWindow;
-        }
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            _mainWindow.Show();  // отображаем главное окно на экране
-            base.OnStartup(e);
-        }
+        
     }
 }
