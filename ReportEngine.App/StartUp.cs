0cadafc930cbe3f0da64518ee8f3df42bef0bf07
DaniilTiunov <114344853+DaniilTiunov@users.noreply.github.com>
2025-07-08 14:44:16 +0500

    Тест

--- ReportEngine.App/StartUp.cs
+++ ReportEngine.App/StartUp.cs
@@ -15,6 +15,7 @@ namespace ReportEngine.App
         {
             try
             {
+
                 var connString = JsonHandler.GetConnectionString(DirectoryHelper.GetConfigPath());// Получаем строку подключения из json файла
 
                 Log.Logger = LoggerConfig.InitializeLogger(); // Конфигурация Serilog
