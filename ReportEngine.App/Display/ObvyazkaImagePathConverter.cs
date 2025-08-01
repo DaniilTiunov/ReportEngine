c668f984d6682f8fab3acffb14b4763ceeb95fa6
DaniilTiunov <114344853+DaniilTiunov@users.noreply.github.com>
2025-08-01 14:55:48 +0500

    Изменения в разметке и добавление конвертера изображений
    В файле `ObvyazkiView.xaml` изменены размеры окна на 1400x700, добавлен ресурс `ObvyazkaImagePathConverter` для преобразования пути к изображению. Обновлены колонки в `DataGrid`, добавлены новые столбцы, включая столбец с изображением, и изменены заголовки существующих столбцов. Контекстное меню было удалено и добавлено обратно в конце определения `DataGrid`.
    
    В файле `ObvyazkaImagePathConverter.cs` добавлен класс `ObvyazkaImagePathConverter`, реализующий интерфейс `IValueConverter`, с методом `Convert` для преобразования имени изображения в путь. Метод `ConvertBack` не реализован и выбрасывает исключение `NotImplementedException`.

---
+++ ReportEngine.App/Display/ObvyazkaImagePathConverter.cs
@@ -0,0 +1,18 @@
+using ReportEngine.Shared.Config.Directory;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ReportEngine.App.Display
+{
+    public class ObvyazkaImagePathConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string imageName && !string.IsNullOrEmpty(imageName))
+                return DirectoryHelper.GetImagesPath(imageName);
+            return null;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
