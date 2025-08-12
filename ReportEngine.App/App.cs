c584d889f406d70530c42c86f764de0c932355f4
DaniilTiunov <114344853+DaniilTiunov@users.noreply.github.com>
2025-08-12 16:56:47 +0500

    Добавление поддержки MaterialDesignExtensions и улучшение UI

--- ReportEngine.App/App.cs
+++ ReportEngine.App/App.cs
@@ -4,6 +4,19 @@ namespace ReportEngine.App
 {
     public partial class App : Application
     {
+        public App()
+        {
+            //var themeDict = new ResourceDictionary
+            //{
+            //    Source = new Uri("pack://application:,,,/Wpf.Ui;component/Resources/Theme/Light.xaml", UriKind.Absolute)
+            //};
+            //var controlsDict = new ResourceDictionary
+            //{
+            //    Source = new Uri("pack://application:,,,/Wpf.Ui;component/Resources/Wpf.Ui.xaml", UriKind.Absolute)
+            //};
 
+           // Resources.MergedDictionaries.Add(themeDict);
+            //Resources.MergedDictionaries.Add(controlsDict);
+        }
     }
 }
