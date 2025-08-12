bd747b5733a8a2c987c3f88e2c416abcc8babb88
DaniilTiunov <114344853+DaniilTiunov@users.noreply.github.com>
2025-08-12 13:17:28 +0500

    Обновление тем и стилей интерфейса

--- ReportEngine.App/Views/MainWindow.xaml.cs
+++ ReportEngine.App/Views/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using ReportEngine.Shared.Config.JsonHelpers;
 using ReportEngine.Shared.Helpers;
 using System.Diagnostics;
 using System.Windows;
+using System.Windows.Controls;
 using AboutProgram = ReportEngine.App.Views.Windows.AboutProgram;
 
 
@@ -21,9 +22,10 @@ namespace ReportEngine.App
             InitializeComponent();
             DataContext = mainViewModel;
 
+            StandartTheme(null, null);
             mainViewModel.OnShowAllProjectsCommandExecuted(null);
             mainViewModel.OnChekDbConnectionCommandExecuted(null);
-        }       
+        }
 
         private void CheckForUpdates(object sender, RoutedEventArgs e)
         {
@@ -53,6 +55,34 @@ namespace ReportEngine.App
         {
             Process.Start("notepad.exe");
         }
-        
+
+        private void ChangeDarkTheme(object sender, RoutedEventArgs e)
+        {
+            var uri = new Uri("/Resources/Dictionaries/DarkTheme.xaml", UriKind.Relative);
+            ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+        }
+        private void ChangeLightTheme(object sender, RoutedEventArgs e)
+        {
+            var uri = new Uri("/Resources/Dictionaries/LightTheme.xaml", UriKind.Relative);
+            ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+        }
+        private void StandartTheme(object sender, RoutedEventArgs e)
+        {
+            var uri = new Uri("/Resources/Dictionaries/GigaChadUI.xaml", UriKind.Relative);
+            ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+        }
+        private void TestTheme(object sender, RoutedEventArgs e)
+        {
+            var uri = new Uri("/Resources/Dictionaries/TestTheme.xaml", UriKind.Relative);
+            ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+        }
     }
 }
