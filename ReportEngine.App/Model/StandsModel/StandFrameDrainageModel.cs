b3a407d7a873668bb57bf866c1cf9e5613f916cb
DaniilTiunov <114344853+DaniilTiunov@users.noreply.github.com>
2025-08-14 09:06:28 +0500

    Добавлен проект ReportEngine.Services и новые классы

--- ReportEngine.App/Model/StandsModel/StandFrameDrainageModel.cs
+++ ReportEngine.App/Model/StandsModel/StandFrameDrainageModel.cs
@@ -1,34 +1,76 @@
-﻿using ReportEngine.App.ViewModels;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
+using ReportEngine.App.ViewModels;
+using ReportEngine.Domain.Entities;
 
 namespace ReportEngine.App.Model.StandsModel
 {
     public class StandFrameDrainageModel : BaseViewModel
     {
-        private ObservableCollection<StandFrameModel> _allAvailableFrames = new();
+        // Коллекции для справочника (доступных для выбора)
+        private ObservableCollection<FormedFrame> _allAvailableFrames = new();
         private ObservableCollection<StandDrainageModel> _allAvailableDrainages = new();
-        private StandFrameModel _selectedFrame = new();
-        private StandDrainageModel _selectedDrainage = new();
 
-        public ObservableCollection<StandFrameModel> AllAvailableFrames
+        // Текущие выбранные элементы из справочника
+        private FormedFrame _selectedFrame;
+        private StandDrainageModel _selectedDrainage;
+
+        // Коллекции для хранения выбранных (добавленных к стенду)
+        private ObservableCollection<StandFrameModel> _frames = new();
+        private ObservableCollection<StandDrainageModel> _drainages = new();
+         
+        // Текущие выбранные элементы из добавленных к стенду
+        private StandFrameModel _selectedStandFrame;
+        private StandDrainageModel _selectedStandDrainage;
+
+        // Свойства для коллекций доступных элементов (справочник)
+        public ObservableCollection<FormedFrame> AllAvailableFrames
         {
             get => _allAvailableFrames;
             set => Set(ref _allAvailableFrames, value);
-        }
-        public StandFrameModel SelectedFrame
-        {
-            get => _selectedFrame;
-            set => Set(ref _selectedFrame, value);
         }
         public ObservableCollection<StandDrainageModel> AllAvailableDrainages
         {
             get => _allAvailableDrainages;
             set => Set(ref _allAvailableDrainages, value);
         }
+
+        // Свойства для текущих выбранных элементов из справочника
+        public FormedFrame SelectedFrame
+        {
+            get => _selectedFrame;
+            set => Set(ref _selectedFrame, value);
+        }
         public StandDrainageModel SelectedDrainage
         {
             get => _selectedDrainage;
             set => Set(ref _selectedDrainage, value);
         }
+
+        // Свойства для коллекций элементов, добавленных к стенду
+        public ObservableCollection<StandFrameModel> Frames
+        {
+            get => _frames;
+            set => Set(ref _frames, value);
+        }
+        public ObservableCollection<StandDrainageModel> Drainages
+        {
+            get => _drainages;
+            set => Set(ref _drainages, value);
+        }
+
+        // Свойства для текущих выбранных элементов из добавленных к стенду
+        public StandFrameModel SelectedStandFrame
+        {
+            get => _selectedStandFrame;
+            set => Set(ref _selectedStandFrame, value);
+        }
+        public StandDrainageModel SelectedStandDrainage
+        {
+            get => _selectedStandDrainage;
+            set => Set(ref _selectedStandDrainage, value);
+        }
+
+        // Свойство для создания нового дренажа вручную
+        public StandDrainageModel NewDrainage { get; set; } = new();
     }
 }
---
+++ ReportEngine.Services/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using ReportEngine.Domain.Entities;
+
+namespace ReportEngine.Services.Interfaces
+{
+    public interface IUserService<TUser>
+        where TUser : User
+    {
+        Task<IEnumerable<TUser>> GetAllUsersAsync();
+        Task<TUser> GetUserByIdAsync(int id);
+        Task<TUser> CreateUserAsync(TUser entity);
+        Task<TUser> UpdateUserAsync(TUser entity);
+        Task DeleteUserAsync(User entity);
+    }
+}
