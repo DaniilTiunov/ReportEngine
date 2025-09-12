5d424a54100b9ad24cca6afce3d1fbe21b4e103e
DaniilTiunov <114344853+DaniilTiunov@users.noreply.github.com>
2025-09-12 12:31:03 +0500

    Обновление UI
    Выбор комплектух для обвязок теперь привязан к событию дабл клика

--- ReportEngine.App/Views/Windows/GenericEquipView.xaml.cs
+++ ReportEngine.App/Views/Windows/GenericEquipView.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using ReportEngine.App.AppHelpers;
+using ReportEngine.App.ViewModels;
+using ReportEngine.Domain.Entities.BaseEntities;
+using ReportEngine.Domain.Entities.BaseEntities.Interface;
+using System.Windows;
+using System.Windows.Input;
 
 namespace ReportEngine.App.Views.Windows;
 
@@ -10,5 +15,20 @@ public partial class GenericEquipView : Window
     public GenericEquipView()
     {
         InitializeComponent();
+    }
+
+    private void SelectEquip_DoubleClick(object sender, MouseButtonEventArgs e)
+    {
+        ExceptionHelper.SafeExecute( () =>
+        {
+            var type = DataContext.GetType();
+
+            if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(GenericEquipViewModel<>))
+            {
+                dynamic vm = DataContext;
+                vm.SelectCommand.Execute(null);
+            }
+
+        });
     }
 }
