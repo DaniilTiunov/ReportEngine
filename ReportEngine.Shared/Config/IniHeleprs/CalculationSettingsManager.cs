daf2ef139980134bfc94ab3bc64e57492a531f76
DaniilTiunov <114344853+DaniilTiunov@users.noreply.github.com>
2025-09-16 15:52:31 +0500

    Обновление настроек

--- ReportEngine.Shared/Config/IniHeleprs/CalculationSettingsManager.cs
+++ ReportEngine.Shared/Config/IniHeleprs/CalculationSettingsManager.cs
@@ -10,6 +10,7 @@ namespace ReportEngine.Shared.Config.IniHeleprs
     {
         private static readonly FileIniDataParser _parser = new ();
         private static readonly string _iniFile = DirectoryHelper.GetIniConfigPath();
+        private static readonly CultureInfo _csvCulture = CultureInfo.GetCultureInfo("ru-RU");
 
         // Синхронные методы
         public static HumanCostSettings LoadHumanCostSettings()
@@ -50,21 +51,28 @@ namespace ReportEngine.Shared.Config.IniHeleprs
             if (costData == null)
                 return new HumanCostSettings();
 
+            double Parse(string? s)
+            {
+                if (string.IsNullOrWhiteSpace(s))
+                    return 0.0;
+                return double.TryParse(s, NumberStyles.Float | NumberStyles.AllowThousands, _csvCulture, out var v) ? v : 0.0;
+            }
+
             return new HumanCostSettings
             {
-                ObvzyakaProduction = Convert.ToDouble(costData["HumanCostSettings"]["obvzyakaProduction"], CultureInfo.InvariantCulture),
-                CollectorProduction = Convert.ToDouble(costData["HumanCostSettings"]["collectorProduction"], CultureInfo.InvariantCulture),
-                Tests = Convert.ToDouble(costData["HumanCostSettings"]["tests"], CultureInfo.InvariantCulture),
-                CommonCheckStand = Convert.ToDouble(costData["HumanCostSettings"]["commonCheckStand"], CultureInfo.InvariantCulture),
-                TimeForCheckStand = Convert.ToDouble(costData["HumanCostSettings"]["timeForCheckStand"], CultureInfo.InvariantCulture),
-                TimeForFinalWork = Convert.ToDouble(costData["HumanCostSettings"]["timeForFinalWork"], CultureInfo.InvariantCulture),
-                TimeForOneDrill = Convert.ToDouble(costData["HumanCostSettings"]["timeForOneDrill"], CultureInfo.InvariantCulture),
-                TimeForCollectorBoil = Convert.ToDouble(costData["HumanCostSettings"]["timeForCollectorBoil"], CultureInfo.InvariantCulture),
-                TimeForAllChecks = Convert.ToDouble(costData["HumanCostSettings"]["timeForAllChecks"], CultureInfo.InvariantCulture),
-                TimeForPrepareAllEquipment = Convert.ToDouble(costData["HumanCostSettings"]["timeForPrepareAllEquipment"], CultureInfo.InvariantCulture)       ,
-                TimeForDrillOneBus = Convert.ToDouble(costData["HumanCostSettings"]["timeForDrillOneBus"], CultureInfo.InvariantCulture),
-                TimeForMontageOneInput = Convert.ToDouble(costData["HumanCostSettings"]["timeForMontageOneInput"], CultureInfo.InvariantCulture),
-                TimeForOthersOperations = Convert.ToDouble(costData["HumanCostSettings"]["timeForOthersOperations"], CultureInfo.InvariantCulture)
+                ObvzyakaProduction = Parse(costData["HumanCostSettings"]["obvzyakaProduction"]),
+                CollectorProduction = Parse(costData["HumanCostSettings"]["collectorProduction"]),
+                Tests = Parse(costData["HumanCostSettings"]["tests"]),
+                CommonCheckStand = Parse(costData["HumanCostSettings"]["commonCheckStand"]),
+                TimeForCheckStand = Parse(costData["HumanCostSettings"]["timeForCheckStand"]),
+                TimeForFinalWork = Parse(costData["HumanCostSettings"]["timeForFinalWork"]),
+                TimeForOneDrill = Parse(costData["HumanCostSettings"]["timeForOneDrill"]),
+                TimeForCollectorBoil = Parse(costData["HumanCostSettings"]["timeForCollectorBoil"]),
+                TimeForAllChecks = Parse(costData["HumanCostSettings"]["timeForAllChecks"]),
+                TimeForPrepareAllEquipment = Parse(costData["HumanCostSettings"]["timeForPrepareAllEquipment"]),
+                TimeForDrillOneBus = Parse(costData["HumanCostSettings"]["timeForDrillOneBus"]),
+                TimeForMontageOneInput = Parse(costData["HumanCostSettings"]["timeForMontageOneInput"]),
+                TimeForOthersOperations = Parse(costData["HumanCostSettings"]["timeForOthersOperations"])
             };
         }
         // Запись в файл
@@ -73,19 +81,21 @@ namespace ReportEngine.Shared.Config.IniHeleprs
             if (costData == null)
                 return;
 
-            costData["HumanCostSettings"]["obvzyakaProduction"] = settings.ObvzyakaProduction.ToString();
-            costData["HumanCostSettings"]["collectorProduction"] = settings.CollectorProduction.ToString();
-            costData["HumanCostSettings"]["tests"] = settings.Tests.ToString();
-            costData["HumanCostSettings"]["commonCheckStand"] = settings.CommonCheckStand.ToString();
-            costData["HumanCostSettings"]["timeForCheckStand"] = settings.TimeForCheckStand.ToString();
-            costData["HumanCostSettings"]["timeForFinalWork"] = settings.TimeForFinalWork.ToString();
-            costData["HumanCostSettings"]["timeForOneDrill"] = settings.TimeForOneDrill.ToString();
-            costData["HumanCostSettings"]["timeForCollectorBoil"] = settings.TimeForCollectorBoil.ToString();
-            costData["HumanCostSettings"]["timeForAllChecks"] = settings.TimeForAllChecks.ToString();
-            costData["HumanCostSettings"]["timeForPrepareAllEquipment"] = settings.TimeForPrepareAllEquipment.ToString();
-            costData["HumanCostSettings"]["timeForDrillOneBus"] = settings.TimeForDrillOneBus.ToString();
-            costData["HumanCostSettings"]["timeForMontageOneInput"] = settings.TimeForMontageOneInput.ToString();
-            costData["HumanCostSettings"]["timeForOthersOperations"] = settings.TimeForOthersOperations.ToString();
+            string F(double v) => v.ToString(_csvCulture);
+
+            costData["HumanCostSettings"]["obvzyakaProduction"] = F(settings.ObvzyakaProduction);
+            costData["HumanCostSettings"]["collectorProduction"] = F(settings.CollectorProduction);
+            costData["HumanCostSettings"]["tests"] = F(settings.Tests);
+            costData["HumanCostSettings"]["commonCheckStand"] = F(settings.CommonCheckStand);
+            costData["HumanCostSettings"]["timeForCheckStand"] = F(settings.TimeForCheckStand);
+            costData["HumanCostSettings"]["timeForFinalWork"] = F(settings.TimeForFinalWork);
+            costData["HumanCostSettings"]["timeForOneDrill"] = F(settings.TimeForOneDrill);
+            costData["HumanCostSettings"]["timeForCollectorBoil"] = F(settings.TimeForCollectorBoil);
+            costData["HumanCostSettings"]["timeForAllChecks"] = F(settings.TimeForAllChecks);
+            costData["HumanCostSettings"]["timeForPrepareAllEquipment"] = F(settings.TimeForPrepareAllEquipment);
+            costData["HumanCostSettings"]["timeForDrillOneBus"] = F(settings.TimeForDrillOneBus);
+            costData["HumanCostSettings"]["timeForMontageOneInput"] = F(settings.TimeForMontageOneInput);
+            costData["HumanCostSettings"]["timeForOthersOperations"] = F(settings.TimeForOthersOperations);
 
             _parser.WriteFile(_iniFile, costData);
         }
