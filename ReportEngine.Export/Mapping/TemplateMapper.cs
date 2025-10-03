6a0cd50dad42fc30b41ab78546d6202896565fda
DaniilTiunov <114344853+DaniilTiunov@users.noreply.github.com>
2025-10-03 13:49:46 +0500

    Обновление маппера
    Обновление маппера

--- ReportEngine.Export/Mapping/TemplateMapper.cs
+++ ReportEngine.Export/Mapping/TemplateMapper.cs
@@ -1,15 +1,37 @@
-﻿namespace ReportEngine.Export.Mapping
+﻿using ReportEngine.Domain.Entities;
+
+namespace ReportEngine.Export.Mapping
 {
     public static class TemplateMapper
     {
-        public readonly static Dictionary<string, object> PassportMapping = new() {
-            { "stand_KKS_code", "KKS-код стенда" },
-            { "stand_Name", "Наименование стенда" },
-            { "stand_Manufacturer", "Изготовитель стенда" },
-            { "stand_SerialNumber", "Заводской номер стенда" },
-            { "stand_YearManufacture", "Год изготовления стенда" },
-            { "stand_Description", "Описание стенда" }
-        };
-    };
-
+        public static Dictionary<string, object> GetPassportMapping(Stand stand)
+        {
+            var standInfo = stand;
+            
+            return new Dictionary<string, object>
+            {
+                { "stand_KKS_code", standInfo?.KKSCode?? "N/A" },
+                { "stand_Name", standInfo?.Design ?? "N/A" },
+                { "stand_Manufacturer", "Изготовитель стенда?? Хз где брать" ?? "N/A" },
+                { "stand_SerialNumber", standInfo?.SerialNumber ?? "N/A" },
+                { "stand_YearManufacture", "Год изготовления стенда?? Хз где брать" ?? "N/A" },
+                { "stand_Description", standInfo?.DesigneStand ?? "N/A" }
+            };
+        }
+        
+        public static Dictionary<string, object> GetTechnologicalCardsMapping(Stand stand)
+        {
+            var standInfo = stand;
+            
+            return new Dictionary<string, object>
+            {
+                { "stand_KKS_code", standInfo?.KKSCode?? "N/A" },
+                { "stand_Name", standInfo?.Design ?? "N/A" },
+                { "stand_Manufacturer", "Изготовитель стенда?? Хз где брать" ?? "N/A" },
+                { "stand_SerialNumber", standInfo?.SerialNumber ?? "N/A" },
+                { "stand_YearManufacture", "Год изготовления стенда?? Хз где брать" ?? "N/A" },
+                { "stand_Description", standInfo?.DesigneStand ?? "N/A" }
+            };
+        }
+    }
 }
