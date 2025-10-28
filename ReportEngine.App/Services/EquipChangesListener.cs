2a33c9bebc3f80ff5badc2636e2f08ff4eb693f2
DaniilTiunov <114344853+DaniilTiunov@users.noreply.github.com>
2025-10-28 17:54:16 +0500

    Заготовка для изменений

--- ReportEngine.App/Services/EquipChangesListener.cs
+++ ReportEngine.App/Services/EquipChangesListener.cs
@@ -3,6 +3,8 @@ using Microsoft.Extensions.Hosting;
 using ReportEngine.Domain.Database.Context;
 using ReportEngine.Shared.Config.Directory;
 using ReportEngine.Shared.Config.JsonHelpers;
+using ReportEngine.Shared.Config.Logger;
+using Serilog;
 
 namespace ReportEngine.App.Services
 {
@@ -10,16 +12,21 @@ namespace ReportEngine.App.Services
     {
         private readonly static string _jsFilePath = DirectoryHelper.GetConfigPath();
         private readonly string _connectionString = JsonHandler.GetConnectionString(_jsFilePath);
-
+        
+        private ILogger _logger;
         private readonly IServiceProvider _serviceProvider;
 
         public EquipChangesListener(IServiceProvider serviceProvider)
         {
+            _logger = LoggerConfig.InitializeLogger().ForContext<EquipChangesListener>();
             _serviceProvider = serviceProvider;
         }
 
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
+
+            _logger.Information("Подписка на изменения стартовала");
+
             var dbConnect = _serviceProvider.GetRequiredService<ReAppContext>();
 
             return Task.CompletedTask;
