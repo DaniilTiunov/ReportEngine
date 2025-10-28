212c13d6c5459d94ed09270f0d7621df1e4f35e8
DaniilTiunov <114344853+DaniilTiunov@users.noreply.github.com>
2025-10-28 13:35:40 +0500

    Обновление рамы
    жопа

---
+++ ReportEngine.Domain/Migrations/20251028083022_UpdateExportDaysFrames.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReportEngine.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateExportDaysFrames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ExportDays",
+                table: "FrameComponents",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExportDays",
+                table: "FrameComponents");
+        }
+    }
+}
