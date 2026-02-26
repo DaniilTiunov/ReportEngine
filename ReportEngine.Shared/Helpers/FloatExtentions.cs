3a22a3496fa0be968f58e34724d9fda03f768680
Anatoliy464 <88888134+Anatoliy464@users.noreply.github.com>
2026-02-26 11:17:35 +0500

    Округление работает

--- ReportEngine.Shared/Helpers/FloatExtentions.cs
+++ ReportEngine.Shared/Helpers/FloatExtentions.cs
@@ -12,15 +12,14 @@ namespace ReportEngine.Shared.Helpers
         {
             return MathF.Ceiling(value);
         }
-
-        public static float Round(this float value, int decimals)
-        {
-            return MathF.Round(value, decimals);
-        }
-
         public static float? Ceiling(this float? value)
         {
             return value.HasValue ? MathF.Ceiling(value.Value) : null;
+        }
+
+        public static float Floor(this float value)
+        {
+            return MathF.Floor(value);
         }
 
         public static float? Floor(this float? value)
@@ -28,10 +27,47 @@ namespace ReportEngine.Shared.Helpers
             return value.HasValue ? MathF.Floor(value.Value) : null;
         }
 
+        public static float Round(this float value, int decimals)
+        {
+            return MathF.Round(value, decimals);
+        }
+
         public static float? Round(this float? value, int decimals)
         {
             return value.HasValue ? (float)MathF.Round(value.Value, decimals) : null;
         }
+
+
+        public static float RoundUp(this float value, int decimals)
+        {
+            float multiplier = MathF.Pow(10, decimals);
+            return MathF.Ceiling(value * multiplier) / multiplier;
+        }
+
+        public static float? RoundUp(this float? value, int decimals)
+        {
+            if (!value.HasValue)
+                return null;
+
+            float multiplier = MathF.Pow(10, decimals);
+            return MathF.Ceiling(value.Value * multiplier) / multiplier;
+        }
+
+        public static float RoundDown(this float value, int decimals)
+        {
+            float multiplier = MathF.Pow(10, decimals);
+            return MathF.Floor(value * multiplier) / multiplier;
+        }
+
+        public static float? RoundDown(this float? value, int decimals)
+        {
+            if (!value.HasValue)
+                return null;
+
+            float multiplier = MathF.Pow(10, decimals);
+            return MathF.Floor(value.Value * multiplier) / multiplier;
+        }
+
 
     }
 }
