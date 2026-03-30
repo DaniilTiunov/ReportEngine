9427cb1089d97e30170eda275b912a25d5f358cd
DaniilTiunov <114344853+DaniilTiunov@users.noreply.github.com>
2026-03-30 17:25:45 +0500

    CRUD для Комплектующих параметров

--- ReportEngine.App/Services/Calculation/ParameterGroupService.cs
+++ ReportEngine.App/Services/Calculation/ParameterGroupService.cs
@@ -2,6 +2,7 @@
 using Mapster;
 using ReportEngine.App.Extensions;
 using ReportEngine.App.Model.CalculationModels;
+using ReportEngine.App.Services.Interfaces;
 using ReportEngine.App.ViewModels;
 using ReportEngine.Domain.Entities.CalculationParameters;
 using ReportEngine.Domain.Repositories;
@@ -11,10 +12,14 @@ namespace ReportEngine.App.Services.Calculation;
 public class ParameterGroupService
 {
     private readonly CalculationRepository _calculationRepository;
+    private readonly INotificationService _notificationService;
 
-    public ParameterGroupService(CalculationRepository calculationRepository)
+    public ParameterGroupService(
+        CalculationRepository calculationRepository,
+        INotificationService notificationService)
     {
         _calculationRepository = calculationRepository;
+        _notificationService = notificationService;
     }
 
     public async Task<ObservableCollection<CalculationParameter>> GetParametersAsync(
@@ -25,19 +30,22 @@ public class ParameterGroupService
         return entity.Parameters.ToObservable();
     }
 
-    public async Task UpdateGroupAsync(CalculationParameterType parametersType)
+    public async Task UpdateGroupAsync(CalculationParameterType parametersType, IEnumerable<CalculationParameter> updatedParameters)
     {
-
+        await _calculationRepository.UpdateParametersInGroup(parametersType, updatedParameters);
+        _notificationService.ShowInfo("Параметры успешно обновлены!");
     }
 
     public async Task AddNewParameterAsync(CalculationParameter parameter, CalculationParameterType parametersType)
     {
-
+        await _calculationRepository.AddParameterToGroup(parameter, parametersType);
+        _notificationService.ShowInfo("Параметр успешно добавлен");
     }
 
     public async Task RemoveParameterFromGroupAsync(CalculationParameter parameter,
         CalculationParameterType parametersType)
     {
-
+        await _calculationRepository.DeleteParameterFromGroup(parameter, parametersType);
+        _notificationService.ShowInfo("Параметр успешно удалён");
     }
 }
