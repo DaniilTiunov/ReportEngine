617f08b06d88db0fa958d51298c37a91f6f9e43c
DaniilTiunov <114344853+DaniilTiunov@users.noreply.github.com>
2026-03-30 15:49:27 +0500

    добавление угара

---
+++ ReportEngine.App/Services/Calculation/ParameterGroupService.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.ObjectModel;
+using Mapster;
+using ReportEngine.App.Extensions;
+using ReportEngine.App.Model.CalculationModels;
+using ReportEngine.App.ViewModels;
+using ReportEngine.Domain.Entities.CalculationParameters;
+using ReportEngine.Domain.Repositories;
+
+namespace ReportEngine.App.Services.Calculation;
+
+public class ParameterGroupService
+{
+    private readonly CalculationRepository _calculationRepository;
+
+    public ParameterGroupService(CalculationRepository calculationRepository)
+    {
+        _calculationRepository = calculationRepository;
+    }
+
+    public async Task<ObservableCollection<CalculationParameter>> GetParametersAsync(
+        CalculationParameterType parametersType)
+    {
+        var entity = await _calculationRepository.GetGroupByTypeAsync(parametersType);
+
+        return entity.Parameters.ToObservable();
+    }
+
+    public async Task UpdateGroupAsync(CalculationParameterType parametersType)
+    {
+
+    }
+
+    public async Task AddNewParameterAsync(CalculationParameter parameter, CalculationParameterType parametersType)
+    {
+
+    }
+
+    public async Task RemoveParameterFromGroupAsync(CalculationParameter parameter,
+        CalculationParameterType parametersType)
+    {
+
+    }
+}
