de28238d1ea3140cba0b794b06c70a8ac6dd8205
DaniilTiunov <114344853+DaniilTiunov@users.noreply.github.com>
2026-08-31 14:29:01 +0500

    Пред релиз

--- ReportEngine.AtomicServices/Models/AtomicProjectModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ReportEngine.AtomicServices.Models;
-
-public class AtomicProjectModel
-{
-    public int Id { get; set; }
-    public int Number { get; set; }
-    public string Description { get; set; }
-}
